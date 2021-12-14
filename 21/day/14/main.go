bab8adae4ae204fc301519ee65ad9c87c3fbbb06
bipol <bipolalam@gmail.com>
2021-12-14 12:56:09 -0500

    day14, runes more performant but still wrong

--- 21/day/14/main.go
+++ 21/day/14/main.go
@@ -9,44 +9,44 @@ import (
 )
 
 func main() {
-	dat, err := os.ReadFile("./puzzle.txt")
+	dat, err := os.ReadFile("./test.txt")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	rows := strings.Split(strings.TrimSpace(string(dat)), "\n")
-	template := strings.Split(rows[0], "")
+	template := []rune(rows[0])
 
 	pairInsertions := rows[2:]
 
-	rules := make(map[string]string)
+	rules := make(map[string]rune)
 	for _, k := range pairInsertions {
 		split := strings.Fields(k)
-		rules[split[0]] = split[2]
+		rules[split[0]] = rune(split[2][0])
 	}
 
 	fmt.Println(template)
 	fmt.Println(rules)
 
+	elementCount := make(map[string]int)
+	for _, k := range template {
+		elementCount[string(k)]++
+
+	}
 	for x := 1; x <= 10; x++ {
-		new := []string{template[0]}
+		new := []rune{template[0]}
 		for y := 1; y < len(template); y++ {
-			pair := fmt.Sprintf("%s%s", template[y-1], template[y])
+			pair := fmt.Sprintf("%s%s", string(template[y-1]), string(template[y]))
 			rule := rules[pair]
-			//fmt.Println("insert", rule, "between", template[y-1], template[y])
+			elementCount[string(rule)]++
 			new = append(new, rule)
 			new = append(new, template[y])
-			//			fmt.Println(new)
 		}
 		template = new
 		fmt.Println(x, "tempate size", len(template))
 	}
 
-	elementCount := make(map[string]int)
-	for _, elem := range template {
-		elementCount[elem]++
-	}
 	sorted := []int{}
 	for _, v := range elementCount {
 		sorted = append(sorted, v)
